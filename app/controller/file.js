4020a57618fe5e5723e143ca6ebbbcc7eba86052
hg1257474 <hg1257474@outlook.com>
2019-10-07 16:45:33 +0800

    7

--- app/controller/file.js
+++ app/controller/file.js
@@ -10,26 +10,7 @@ module.exports = app => {
     // }
 
     async upload() {
-      const { ctx } = this;
-      const file = ctx.request.files[0];
-      const name = 'egg-multipart-test/' + path.basename(file.filename);
-      let result;
-      try {
-        // process file or upload to cloud storage
-        result = await ctx.oss.put(name, file.filepath);
-      } finally {
-        // remove tmp files and don't block the request's response
-        // cleanupRequestFiles won't throw error even remove file io error happen
-        ctx.cleanupRequestFiles();
-        // remove tmp files before send response
-        // await ctx.cleanupRequestFiles();
-      }
-
-      ctx.body = {
-        url: result.url,
-        // get all field values
-        requestBody: ctx.request.body
-      };
+      console.log('hello');
     }
   }
   return Controller;
